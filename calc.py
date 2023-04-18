a8de02c93770f522ddbe9700ffb4fdfbbea36243
Giovanni Leandri <Giovanni.Leandri@monash.edu>
2023-04-18 11:33:46 +1000

    convert strings to integers

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
+num0, num = int(num), int(num0)   
     if operator == '+':
         return num0 + num1
     elif operator == '-':
